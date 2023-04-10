e9e33b40290cd948fb55bd605bd76c52688791aa
Bruno <bruno_french@outlook.com>
2023-04-10 08:28:56 -0400

    feat: add gravity mechanics to scene

---
+++ SSC2023.swiftpm/Boxes.swift
@@ -0,0 +1,25 @@
+//
+//  Boxes.swift
+//  SSC2023
+//
+//  Created by Bruno França do Prado on 04/04/23.
+//
+
+import Foundation
+import SwiftUI
+import SpriteKit
+
+class boxScene: SKScene {
+    override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let box = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
+        box.position = location
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+        addChild(box)
+    }
+}
--- SSC2023.swiftpm/ContentView.swift
+++ SSC2023.swiftpm/ContentView.swift
@@ -1,12 +1,25 @@
 import SwiftUI
+import SpriteKit
 
+// A sample SwiftUI creating a GameScene and sizing it
+// at 300x400 points
 struct ContentView: View {
+    var scene: SKScene {
+        let scene = boxScene()
+        scene.size = CGSize(width: 300, height: 400)
+        scene.scaleMode = .fill
+        return scene
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        SpriteView(scene: scene)
+            .frame(width: 300, height: 400)
+            .ignoresSafeArea()
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
